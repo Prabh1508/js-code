c312c26c6cc4e4e73fe97ad806b72245251ea400
Prabh1508 <158257624+Prabh1508@users.noreply.github.com>
2024-02-01 10:15:35 +0000

    add

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Prabhjot"
+const repoCount = "50"
+
+// console.log(name + repoCount + "value");
+// console.log("Hello my name is ${name} and my repocount is ${repoCount}")
+
+const gameName = new String("prabhjotpsc")
